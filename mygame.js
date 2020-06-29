6bd3dc9d9be6597a9a65ccd2ab35ac451cae074d
Mohamed Elshawaf <m_shawaf@outlook.com>
2020-06-29 14:15:31 +0200

    bouncing

--- mygame.js
+++ mygame.js
@@ -1,10 +1,11 @@
 var sqrInitY;
-var accVal = 1;
+var lineY;
 
 class MyGame {
     setup() {
         background(51);
-        sqrInitY = height / 2 - 13;
+        lineY = height - 20;
+        sqrInitY = lineY - 13;
         this.pos = createVector(20, sqrInitY);
         this.vel = createVector(0, 0);
         this.acc = createVector(0, 0);
@@ -14,30 +15,29 @@ class MyGame {
 
     play() {
         background(51);
-        line(0, height / 2, width, height / 2);
+        line(0, lineY, width, lineY);
         square(this.pos.x, this.pos.y, 10);
 
-        let gravity = createVector(0, 2);
+        let gravity = createVector(0, .5);
         this.applyForce(gravity);
 
         this.update();
         this.edges();
 
         if (keyIsDown(UP_ARROW)) {
-            this.up();
+            var v = createVector(0, -1.5);
+            // this.acc.add(v);
+            this.vel.add(this.acc);
+            this.pos.add(this.vel);
+            this.acc.add(gravity);
+            this.vel.add(this.acc);
+            this.pos.add(this.vel);
         }
     }
 
     applyForce(force) {
         this.acc.add(force);
     }
-
-    up() {
-        this.acc.add(createVector(0, 2));
-        this.vel.add(this.acc);
-        this.pos.add(this.vel);
-    }
-
 
     update() {
         this.vel.add(this.acc);
@@ -46,22 +46,10 @@ class MyGame {
         square(this.pos.x, this.pos.y, 10);
     }
 
-    keyReleased() {
-        this.acc.add(createVector(0, 5));
-        this.vel.add(this.acc);
-        this.pos.add(this.vel);
-        this.edges();
-    }
-
     edges() {
-        if (this.pos.y >= height / 2 - 13) {
-            this.pos.y = height / 2 - 13;
+        if (this.pos.y >= sqrInitY) {
+            this.pos.y = sqrInitY;
             this.vel.y *= -1;
         }
     }
-}
-
-
-function keyReleased() {
-    return game.keyReleased(); // prevent any default behavior
 }
